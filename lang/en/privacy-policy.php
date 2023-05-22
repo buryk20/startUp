b108526739346e81de4293941a482c85ecf1cb32
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-05-22 14:18:32 +0300

    private

---
+++ lang/en/privacy-policy.php
@@ -0,0 +1,53 @@
+<?php
+return [
+    'title' => 'Privacy Policy',
+    'title_seo' => 'Privacy Policy - StarUpGroup',
+
+    'item_1' => 'The visitor may not misuse this site. In particular, the visitor must not influence and interfere with the operation of this site or access the information published on this site, other than through a standard interface.',
+    'item_2' => "The visitor must use this site exclusively in accordance with the current legislation of Ukraine. If the site administration detects a violation of this PRIVACY POLICY AND TERMS and CONDITIONS by the visitor, or suspects the visitor of this, it may suspend or completely close the visitor's access to the site.",
+    'item_3' => 'Burtsev Sergey Nikolaevich may revise this PRIVACY POLICY AND TERMS at any time.',
+    'item_4' => 'Collection and use of personal data:',
+
+    'item_4_1' => 'Personal data is information or a set of information about a natural person who is identified or can be identified.',
+    'item_4_2' => "Personal data can be obtained through various communication channels. In particular, through the service of website visitors, in writing or electronically on the website.",
+    'item_4_3' => "The visitor to the website https://star-up-group.com.ua/ may be asked to enter personal data with his consent. In particular, you may be asked to enter your first name, last name, email address, phone number, date of birth, gender, as well as to register on the site using social networks such as Twitter, Facebook. Burtsev Sergey Nikolaevich may contact the website visitor with a request to provide other data.",
+    'item_4_4' => "When registering on the website https://star-up-group.com.ua/, the visitor consents to the processing of his personal and registration data by Burtsev Sergey Nikolaevich. If the visitor decides to provide his personal data, he hereby gives his consent to the transfer and storage of this data.",
+    'item_4_5' => 'Visitors" personal data is used to ensure the exchange of information, relations in the field of advertising and communication in accordance with the Laws of Ukraine "On Personal Data Protection", "On Information", "On Advertising", "On Telecommunications", "On Entrepreneurship", "On Information Protection in information and telecommunication systems", ND TZI 2.5-010-03 "Requirements for protection of WEB page information against unauthorized access".',
+    'item_4_6' => "Personal data of visitors can be stored for 3 years after the subject of personal data ceased to be a visitor of the website https://star-up-group.com.ua/.",
+    'item_4_7' => "Burtsev Sergey Nikolaevich server temporarily registers the domain name or IP address of the visitor's computer, as well as the access date, files (file name and URL), HTTP response code, and the website from which the visitor got to https://logiclink. com.ua/, the number of bytes transferred during the session.",
+    'item_4_8' => "Cookies are used during the operation of the website https://star-up-group.com.ua/. With their help, the visitor's browser can be identified. User settings and other information can be saved in these files. The visitor can configure the browser to reject all cookies or to notify when they are sent.",
+    'item_4_9' => 'Cookies provide support for security features and startup. Cookies also allow us to track violations of the PRIVACY POLICY AND TERMS by visitors or devices. Cookies help to estimate the number and frequency of requests, as well as detect and block visitors or devices that attempt to perform batch downloads of information from the website.',
+    'item_4_10' => "With the help of the Google Analytics system, Burtsev Sergey Nikolaevich collects statistics about visiting the website https://star-up-group.com.ua/, such as the pages visited, the number of page views, the information that was downloaded, the domains of Internet providers and the countries of origin of visitors, as well as addresses of websites visited before and after https://star-up-group.com.ua/ etc. None of these actions are associated with the visitor personally and are measured only in aggregate.",
+    'item_4_11' => 'Persons under the age of 18 do not have the right to use any services on the website https://star-up-group.com.ua/, including registration. Burtsev Sergey Nikolaevich will never intentionally collect data about minors, use it, or disclose it to a third party without appropriate permission. Burtsev Sergey Nikolaevich advises parents and guardians to monitor which sites their children visit.',
+
+    'item_5' => 'The website https://star-up-group.com.ua/ may contain links to sites that are not managed by Burtsev Sergey Nikolaevich. Such links are provided for informational purposes only.',
+
+    'item_6' => 'The website https://star-up-group.com.ua/ can use the Google Project Shield protection service against DDOS attacks. This means that Google may process the data collected during the operation of Project Shield in accordance with the privacy policy of Goolge Inc.',
+    'item_6_1' => 'These modules can be account synchronization buttons on the website https://star-up-group.com.ua/: Like, tell or, respectively, "I like". If the visitor opened one of the web pages equipped with such a plug-in, his internet browser would connect him directly to the servers of Facebook, Twitter, LinkedIn, Google+ or Youtube. The plugin will transmit to the server data about which web pages of the site https://star-up-group.com.ua/ the visitor viewed.',
+    'item_6_2' => "When using any of the plugin features, this information will also be synchronized with the visitor's Facebook, Twitter, LinkedIn, Google+ or Youtube account. More detailed information about the collection and use of data by Facebook, Twitter, LinkedIn, Google+ or Youtube, as well as the rights and options for the protection of personal data in this context, can be found in the privacy section of the Facebook, Twitter, LinkedIn, Google+ or Youtube websites.",
+    'item_6_3' => "In order to avoid connection of Facebook, Twitter, LinkedIn, Google+ or Youtube networks with the site https://star-up-group.com.ua/ and further correlation of this information with the account, the visitor must log out of the network account before visiting the site https://star-up-group.com.ua/.",
+
+    'item_7' => 'Information about traffic passing through the Blehman Oksana Petrivna network, e-mail and customer activity on the Internet on the website https://star-up-group.com.ua/ is protected in accordance with the law. This means that Burtsev Sergey Nikolaevich and his employees cannot interfere in the secret of telecommunications.',
+
+    'item_8' => 'Burtsev Sergey Nikolaevich has the right to provide any personal information of visitors to https://star-up-group.com.ua/ at the request of law enforcement agencies, at the decision/order of the court or as part of other legal procedures, or at another request in accordance with the current legislation of Ukraine.',
+
+    'item_9' => 'Burtsev Sergey Nikolaevich does not guarantee and does not claim that the use of the material provided on the website https://star-up-group.com.ua/ will not harm the interests of third parties.',
+
+    'item_10' => "Burtsev Sergey Nikolaevich does not guarantee and does not claim that the use of the material provided on the website https://star-up-group.com.ua/ will not harm the interests of third parties.",
+
+    'item_11' => "The author's materials published on the website https://star-up-group.com.ua/ are the point of view of journalists, are not the norm and should not be interpreted as such. Burtsev Sergey Nikolaevich is not responsible for the accuracy, objectivity and relevance of information published on the website https://star-up-group.com.ua/, in particular, with reference to other sources",
+
+    'item_12' => 'All rights to the site https://star-up-group.com.ua/ and its content belong to Burtsev Sergey Nikolaevich or are used in agreement with the rights holders. This site in general and its individual elements are protected by the Law of Ukraine "On Copyright and Related Rights", international legal acts and other acts of current legislation of Ukraine.',
+
+    'item_13' => 'Site visitors must not distribute, modify, transfer, use or re-use any information from https://star-up-group.com.ua/ for any public or commercial purpose without the written permission of Burtsev Sergey Nikolaevich. Visitors must comply with all copyright and other proprietary notices on downloaded and/or copied materials. The copied information must contain an active link to the site https://star-up-group.com.ua/.',
+
+    'item_14' => 'Copying information from the website https://star-up-group.com.ua/ can be done for non-commercial purposes for posting on user pages, blogs, and social networks.',
+
+    'item_15' => "The use of the site https://star-up-group.com.ua/ by the visitor is at the user's own risk. Burtsev Sergey Nikolaevich or any other party involved in the creation, production or maintenance of the website https://star-up-group.com.ua/ is not responsible for direct, indirect or accidental damages, at any time caused or caused by the fact that the visitor had access to the website https://star-up-group.com.ua/, used it or hoped for it, even if Blekhman Oksana Petrivna warned the visitor about the possibility of such damages and losses",
+
+    'item_16' => "Oksana Petrovna Blekhman will in no way be involved in any direct, indirect, intentional or unintentional damage caused by any use of the site https://star-up-group.com.ua/, or on any other interconnected site including, without limitation, any loss of income, goodwill, loss of programs or other data in the visitor's information processing system, even if Blekhman Oksana Petrovna was promptly informed of the possibility of such damage.",
+
+    'item_17' => 'When working with the site https://star-up-group.com.ua/, the visitor is not granted intellectual property rights to any site, nor to its content or the use of any branding elements or logos of the site https://star-up-group.com.ua/.',
+
+    'item_18' => 'If you have visited this site, you automatically agree to this PRIVACY POLICY AND TERMS. If you do not agree with this PRIVACY POLICY AND TERMS, you should leave this site.',
+];
--- views/privacy-policy.blade.php
+++ views/privacy-policy.blade.php
@@ -1,11 +1,115 @@
 @extends('layout')
 
-@section('title'){{ __('blog.metaTitle') }}@endsection
-@section('metaDiscription'){{ __('blog.metaDescription') }}@endsection
+@section('title'){{ __('privacy-policy.title_seo') }}@endsection
+<!-- @section('metaDiscription'){{ __('blog.metaDescription') }}@endsection -->
 
 @section('main_content')
-<main>
-
+<main class="main privacy-policy wrp-margin">
+    <div class="main-wrp">
+        <h1>{{ __('privacy-policy.title') }}</h1>
+        <ol>
+            <li>
+                {{ __('privacy-policy.item_1') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_2') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_3') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_4') }}
+                <ul>
+                    <li>
+                        <span class="privacy-policy__list">4.1</span>{{ __('privacy-policy.item_4_1') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.2</span>{{ __('privacy-policy.item_4_2') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.3</span>{{ __('privacy-policy.item_4_3') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.4</span>{{ __('privacy-policy.item_4_4') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.5</span>{{ __('privacy-policy.item_4_5') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.6</span>{{ __('privacy-policy.item_4_6') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.7</span>{{ __('privacy-policy.item_4_7') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.8</span>{{ __('privacy-policy.item_4_8') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.9</span>{{ __('privacy-policy.item_4_9') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.10</span>{{ __('privacy-policy.item_4_10') }}
+                    </li>
+                    <li>
+                        <span class="privacy-policy__list">4.11</span>{{ __('privacy-policy.item_4_11') }}
+                    </li>
+                </ul>
+            </li>
+            <li>
+                {{ __('privacy-policy.item_5') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_6') }}
+                <ul>
+                    <li>
+                    <span class="privacy-policy__list">6.1</span>{{ __('privacy-policy.item_6_1') }}
+                    </li>
+                    <li>
+                    <span class="privacy-policy__list">6.2</span>{{ __('privacy-policy.item_6_2') }}
+                    </li>
+                    <li>
+                    <span class="privacy-policy__list">6.3</span>{{ __('privacy-policy.item_6_3') }}
+                    </li>
+                </ul>
+            </li>
+            <li>
+                {{ __('privacy-policy.item_7') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_8') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_9') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_10') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_11') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_12') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_13') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_14') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_15') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_16') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_17') }}
+            </li>
+            <li>
+                {{ __('privacy-policy.item_17') }}
+            </li>
+        </ol>
+    </div>
 </main>
 @endsection
 @section('script_tel')

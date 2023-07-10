22c572e066773c062b19c84e706a4efe4cfba89d
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-07-10 18:00:12 +0300

    contentful

--- js/main.js
+++ js/main.js
@@ -26,6 +26,29 @@ import {articleCount} from './articleCount.js';
 articleCount();
 
 
+
+
+const contentful = require('contentful');
+
+    // Замените <CONTENTFUL_SPACE_ID> и <CONTENTFUL_ACCESS_TOKEN> соответствующими значениями
+    const SPACE_ID = '<5f0qcp0syfsf>';
+    const ACCESS_TOKEN = '<G4bxZvMZa5AB58gv1nSbUDbP_rdyT1TMAn57-uZSYoo>';
+    const ORGANIZATION_ID = '3Yx4Fi2moxq2zsWWZ00rG8';
+
+    const client = contentful.createClient({
+    space: SPACE_ID,
+    accessToken: ACCESS_TOKEN
+    });
+
+    client.getOrganization(ORGANIZATION_ID)
+    .then(response => {
+        const organization = response.items[0];
+        console.log(organization);
+        // Обрабатывайте организацию по своему усмотрению
+    })
+    .catch(error => {
+        console.log('Ошибка при получении данных:', error);
+    });
 // import {phoneValidCantry} from './phoneValidCantry.js';
 // phoneValidCantry();
 

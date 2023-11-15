25e7da8ae2e8195a8ca4c4bbb96b9b0d869e770e
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-11-15 18:06:03 +0200

    out-works

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const test = require('./server/test_m.js');
-const cron = require('node-cron');
+// const test = require('./server/test_m.js');
+// const cron = require('node-cron');
 
-// cron.schedule('22 08 * * *', () => {
-    test();
-// })
+// // cron.schedule('22 08 * * *', () => {
+//     test();
+// // })
